a06b39df0627383970dc84550813cd983893715f
Vu Van Hai <HaiVV.SE@gmail.com>
2025-02-19 09:57:19 +0700

    test

---
+++ LaundryService/LaundryService.Api/Controllers/AuthController.cs
@@ -0,0 +1,22 @@
+﻿using LaundryService.Dto.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaundryService.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            if (request.Email == "test@gmail.com" && request.Password == "Password@1")
+            {
+                return Ok(new { Message = "Login successfully" });
+            }
+
+            return Unauthorized(new { Message = "Email or Password is not correct" });
+        }
+    }
+}
--- LaundryService/LaundryService.Api/Program.cs
+++ LaundryService/LaundryService.Api/Program.cs
@@ -7,18 +7,36 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder =>
+        {
+            builder.AllowAnyOrigin()
+                   .AllowAnyHeader()
+                   .AllowAnyMethod();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+app.UseCors("AllowAll");
+
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Laundry Service");
+});
 
 app.MapControllers();
 
