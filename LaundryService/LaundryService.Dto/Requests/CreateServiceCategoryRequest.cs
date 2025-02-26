6104d3f244434c10195fe5ccaaab768c8d78738c
Vu Van Hai <HaiVV.SE@gmail.com>
2025-02-26 14:49:42 +0700

    ServiceCategory

---
+++ LaundryService/LaundryService.Dto/Requests/CreateServiceCategoryRequest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LaundryService.Dto.Requests
+{
+    public class CreateServiceCategoryRequest
+    {
+        [Required(ErrorMessage = "Name is required.")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Icon file is required.")]
+        public IFormFile Icon { get; set; }
+    }
+}
