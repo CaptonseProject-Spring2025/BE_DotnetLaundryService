507f98c572d674cfa4bc4696d42c92232b5d2bd3
Vu Van Hai <HaiVV.SE@gmail.com>
2025-03-12 01:55:57 +0700

    add role to response

--- LaundryService/LaundryService.Dto/Responses/UserDetailResponse.cs
+++ LaundryService/LaundryService.Dto/Responses/UserDetailResponse.cs
@@ -12,6 +12,7 @@ namespace LaundryService.Dto.Responses
         public string? FullName { get; set; }
         public string? PhoneNumber { get; set; }
         public string? Email { get; set; }
+        public string? Role { get; set; }
         public string? Status { get; set; }
         public string? Avatar { get; set; }
         public DateOnly? Dob { get; set; }
