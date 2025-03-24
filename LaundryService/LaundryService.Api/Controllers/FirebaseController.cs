12c1dbc8b63c4e57fa6d4905f50921a006343704
Huy <nhathuy.github@gmail.com>
2025-03-24 13:01:27 +0700

    Save FCMToken to Firebase

---
+++ LaundryService/LaundryService.Api/Controllers/FirebaseController.cs
@@ -0,0 +1,37 @@
+﻿using LaundryService.Domain.Interfaces.Services;
+using LaundryService.Dto.Requests;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LaundryService.Api.Controllers
+{
+    [Route("api/firebase")]
+    [ApiController]
+    public class FirebaseController : BaseApiController
+    {
+        private readonly IFirebaseStorageService _firebaseStorageService;
+
+        public FirebaseController(IFirebaseStorageService firebaseStorageService)
+        {
+            _firebaseStorageService = firebaseStorageService;
+        }
+
+        [HttpPost("save-fcmtoken")]
+        public async Task<IActionResult> SaveToken([FromBody] CreateTokenRequest request)
+        {
+            if (string.IsNullOrEmpty(request.UserId) || string.IsNullOrEmpty(request.FcmToken))
+            {
+                return BadRequest("UserId và Token không được để trống.");
+            }
+
+            try
+            {
+                await _firebaseStorageService.SaveTokenAsync(request.UserId, request.FcmToken);
+                return Ok("FCMToken đã được lưu thành công!");
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"Lỗi khi lưu fcmtoken: {ex.Message}");
+            }
+        }
+    }
+}
