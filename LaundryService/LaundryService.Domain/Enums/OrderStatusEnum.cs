1df2d50264aba1280f749d54450c283aabcaf4bc
Vu Van Hai <HaiVV.SE@gmail.com>
2025-04-03 02:13:46 +0700

    Update OrderStatusEnum.cs

--- LaundryService/LaundryService.Domain/Enums/OrderStatusEnum.cs
+++ LaundryService/LaundryService.Domain/Enums/OrderStatusEnum.cs
@@ -8,19 +8,19 @@ namespace LaundryService.Domain.Enums
 {
     public enum OrderStatusEnum
     {
-        INCART,
-        PENDING,
-        CONFIRMED,
-        SCHEDULED_PICKUP,
-        PICKINGUP,
-        PICKEDUP,
-        CHECKED,
-        WASHING,
-        WASHED,
-        QUALITY_CHECKED,
-        SCHEDULED_DELIVERY,
-        DELIVERING,
-        DELIVERED,
-        CANCELLED
+        INCART = 0,
+        PENDING = 1,
+        CONFIRMED = 2,
+        SCHEDULED_PICKUP = 3,
+        PICKINGUP = 4,
+        PICKEDUP = 5,
+        CHECKED = 6,
+        WASHING = 7,
+        WASHED = 8,
+        QUALITY_CHECKED = 9,
+        SCHEDULED_DELIVERY = 10,
+        DELIVERING = 11,
+        DELIVERED = 12,
+        CANCELLED = 13
     }
 }
